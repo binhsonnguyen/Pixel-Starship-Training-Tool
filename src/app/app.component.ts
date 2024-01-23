fb613ab178f246d73f7e0c07ee25fd1208e38b3e
son <son@codegym.vn>
2024-01-23 13:31:41 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import StatsSet from "pss-training-lib/dist/StatsSet";
+import TrainingTask from "pss-training-lib/dist/TrainingTask";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'pixel-starship-training-tool';
+  totalTrainingPoint: number = 110
+  fatigue: number = 0;
+  currentTraining: StatsSet = new StatsSet();
+  trainingTask: TrainingTask = TrainingTask.ABL_COMMON
 }
