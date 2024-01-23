5804cd46ba34606207371ebae4f060d6d0898a15
son <son@codegym.vn>
2024-01-23 14:35:11 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {FormsModule} from "@angular/forms";
 import StatsSet from "pss-training-lib/dist/StatsSet";
 import TrainingTask from "pss-training-lib/dist/TrainingTask";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {CommonModule} from "@angular/common";
+import {MatInput} from "@angular/material/input";
+import Training from "pss-training-lib/dist/Training";
+import Stat from "pss-training-lib/dist/Stat";
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, MatGridTile, MatGridList, CommonModule, MatInput],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  title: string = "pixel-starship-training-tool"
   totalTrainingPoint: number = 110
   fatigue: number = 0;
   currentTraining: StatsSet = new StatsSet();
   trainingTask: TrainingTask = TrainingTask.ABL_COMMON
+  training: Training = new Training(
+    this.totalTrainingPoint,
+    this.fatigue,
+    this.trainingTask,
+    this.currentTraining
+  )
+
+  ngOnInit(): void {
+    this.currentTraining.hp = 10
+  }
+
+  protected readonly Stat = Stat;
 }
