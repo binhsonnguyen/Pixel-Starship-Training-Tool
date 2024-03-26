32693f3785dd5bdf2f396ed475d09ec84826911a
son <son@codegym.vn>
2024-03-26 08:54:46 +0700

    add stat icon reserver function

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import Stat from "pss-training-lib/dist/Stat";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
+  private static dirPrefix = "assets/fatigue"
 
   constructor() { }
+
+  getStatIcon(stat: Stat) {
+    switch (stat) {
+      case Stat.HP:
+        return `${ImageService.dirPrefix}/HP.webp`
+      case Stat.RPR:
+        return `${ImageService.dirPrefix}/Repair.webp`
+      case Stat.ATK:
+        return `${ImageService.dirPrefix}/Attack.webp`
+      case Stat.ABL:
+        return `${ImageService.dirPrefix}/Ability.webp`
+      case Stat.STA:
+        return `${ImageService.dirPrefix}/Stamina.webp`
+      case Stat.ENG:
+        return `${ImageService.dirPrefix}/Engine.webp`
+      case Stat.PLT:
+        return `${ImageService.dirPrefix}/Pilot.webp`
+      case Stat.SCI:
+        return `${ImageService.dirPrefix}/Science.webp`
+      case Stat.WPN:
+        return `${ImageService.dirPrefix}/Weapon.webp`
+      default:
+        return ""
+    }
+  }
 }
